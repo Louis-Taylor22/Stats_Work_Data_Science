a5a5129b1c257c90ed4701f58fea4d7a1d5643f2
Louis-Taylor22 <louis.taylor@uea.ac.uk>
2023-01-30 16:33:14 +0000

    Compared the groups of data, calculated standard deviation and means and then began to start calculating the difference in means between the two.

--- scripts/12_introduction.R
+++ scripts/12_introduction.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(here)
+library(kableExtra)
 #Load in necessary packages 
 darwin <- read_csv(here("data", "darwin.csv"))
 
@@ -54,3 +55,40 @@ darwin %>%
 ggsave("figures/Basic_Darwin_Data_Visualisation.png", dpi=300)
 
 #---Comparing Groups---
+
+darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+# the code above calculates the standard deviation and the mean for darwin data set - seperately for self and cross
+#--- Converting SD and Mean into figure ---
+# make a new object
+darwin_summary <-darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+
+# make a summary plot
+darwin_summary %>% 
+  ggplot(aes(x=type,
+             y=mean))+
+  geom_pointrange(aes(ymin=mean-sd, ymax=mean+sd))+
+  theme_bw()
+ggsave("figures/Darwin_MeanSD_Figure.png", dpi=300)
+
+# Creates a graph where by central dot represents mean and the shows standard deviation 
+
+#---Creating a new table using Kable---
+
+# use kable extra functions to make a nice table (could be replaced with kable() if needed)
+darwin_summary %>% 
+  kbl(caption="Summary statistics of crossed and selfed maize plants") %>% 
+  kable_styling(bootstrap_options = "striped", full_width = T, position = "left")
+
+#---Estimating the difference of mean height in self and crossed plants---
+# pivot data to wide format then subtract Selfed plant heights from Crossed plant heights
+
+darwin_wide <- darwin %>% 
+  pivot_wider(names_from = type, values_from = height) %>% 
+  mutate(difference = Cross - Self)
+darwin_wide
