8a3c67802e754f4e7a6a3d0d412e295c8e8a2024
Louis-Taylor22 <louis.taylor@uea.ac.uk>
2023-01-30 16:42:35 +0000

    Created a normal distribution plot to learn about standard deviation - calculated confidence intervals for the mean difference

--- scripts/12_introduction.R
+++ scripts/12_introduction.R
@@ -107,4 +107,30 @@ difference_summary %>%
 #the average difference in height was 2.62 ± 1.22 inches (mean ± SE).
 
 
+#--- Calculating error and the significance of the mean difference ---
+#Null hypothesis - there is no difference in the mean height of self vs crossed plants
+#Alternate hypothesis - inbreeding reduces the fitness of the selfed plants, observed as selfed plants on average being smaller than crossed plants
+
+
+# Learning about normal distributions
+#Create a sequence of 100 equally spaced numbers between -4 and 4
+x <- seq(-4, 4, length=100)
+
+#create a vector of values that shows the height of the probability distribution
+#for each value in x
+y <- dnorm(x)
+
+#plot x and y as a scatterplot with connected lines (type = "l") and add
+#an x-axis with custom labels
+plot(x,y, type = "l", lwd = 2, axes = FALSE, xlab = "", ylab = "")
+axis(1, at = -3:3, labels = c("-3s", "-2s", "-1s", "mean", "1s", "2s", "3s"))
+
+lowerCI <- 2.62-(2*1.22)
+
+upperCI <- 2.62+(2*1.22)
+
+lowerCI
+upperCI
+
+#Above is the calculated confidence intervals for the data 
 
