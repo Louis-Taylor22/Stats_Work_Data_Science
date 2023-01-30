ccf210346ce26a10043dc9a5c39311389cc059dc
Louis-Taylor22 <louis.taylor@uea.ac.uk>
2023-01-30 16:20:51 +0000

    Performed some data checking and started visualisation

---
+++ scripts/12_introduction.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(here)
+#Load in necessary packages 
+darwin <- read_csv(here("data", "darwin.csv"))
+
+#--- Checking the data ---
+# check the structure of the data
+glimpse(darwin)
+
+# check data is in a tidy format
+head(darwin)
+
+# check variable names
+colnames(darwin)
+
+
+# clean up column names
+
+darwin <- janitor::clean_names(darwin)
+
+# check for duplication
+darwin %>% 
+  duplicated() %>% 
+  sum()
+
+# check for typos - by looking at impossible values
+darwin %>% 
+  summarise(min=min(height, na.rm=TRUE), 
+            max=max(height, na.rm=TRUE))
+
+# check for typos by looking at distinct characters/values
+
+darwin %>% 
+  distinct(pair)
+
+darwin %>% 
+  distinct(type)
+
+# missing values
+darwin %>% 
+  is.na() %>% 
+  sum()
+
+# quick summary
+
+summary(darwin)
+
+#---Basic visulaisation ---
