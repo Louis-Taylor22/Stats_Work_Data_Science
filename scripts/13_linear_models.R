bdb69ff713ef849b90bed8a72a99660ae181b4e8
Louis-Taylor22 <louis.taylor@uea.ac.uk>
2023-02-20 12:18:23 +0000

    Started to investigate the linear model created using summary functions from R and tidyverse's package called Broom!

--- scripts/13_linear_models.R
+++ scripts/13_linear_models.R
@@ -6,3 +6,15 @@ library(performance)
 #---\-_-/---
 #Creating a linear model 
 lsmodel0 <- lm(formula = height ~ 1, data = darwin)
+#Using summary to investigate the model 
+summary(lsmodel0)
+#Can also use a package called broom to do this provided by tidyverse library
+#See below for that!
+#---/-_-\---
+broom::tidy(lsmodel0)
+#summarises information about model components
+broom::glance(lsmodel0)
+#reports information about the entire model
+broom::augment(lsmodel0)
+#adds information about individual observations to a data set and it can be used
+#to model predictions onto a new dataset
