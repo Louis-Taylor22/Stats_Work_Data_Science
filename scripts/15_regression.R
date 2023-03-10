e96759d2c235d4da2392ad537ef126014a569985
Louis-Tayor22 <Louis.taylor@uea.ac.uk>
2023-03-10 12:19:38 +0000

    Began to discuss regression and imported data needed for regression work

---
+++ scripts/15_regression.R
@@ -0,0 +1,10 @@
+#Moving onto continuous explanatory variables 
+library(tidyverse)
+library(rstatix)
+library(performance)
+#The equation for linear regression model is 
+#Y= a + bx
+#Where Y is the predicted value of the response variable 
+#A is the regression intercept (the value of y when x=0)
+#B is the slope of the regression line 
+#X is the value of the explanatory variable 
